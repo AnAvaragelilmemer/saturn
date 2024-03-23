e705821500c81256f8dc32dcbf40d97dff2e92b8
rtaxevasion <116945939+AnAvaragelilmemer@users.noreply.github.com>
2024-03-23 18:23:20 +0800

    Update Words.lua

--- assets/executorutils/Words.lua
+++ assets/executorutils/Words.lua
@@ -1,54 +1,34 @@
 
 local words = {
-	['print'] = 'builtin',
-	['warn'] = 'builtin',
-	['Vector3'] = 'builtin',
-	['Vector2'] = 'builtin',
-  ['UDim'] = 'builtin',
-  ['UDim2'] = 'builtin',
-	['error'] = 'builtin',
-	['Instance'] = 'builtin',
-	['game'] = 'builtin',
-	['script'] = 'builtin',
-	['workspace'] = 'builtin',
-  ['assert'] = 'builtin',
-  ['task'] = 'builtin',
-  ['wait'] = 'builtin',
-  ['pairs'] = 'builtin',
-  ['ipairs'] = 'builtin',
-	['next'] = 'builtin',
-  ['loadstring'] = 'builtin',
-  ['Enum'] = 'builtin',
-  ['TweenInfo'] = 'builtin',
-	['while'] = 'keyword',
-	['true'] = 'keyword',
-	['false'] = 'keyword',
-	['then'] = 'keyword',
-	['do'] = 'keyword',
-	['if'] = 'keyword',
-  ['end'] = 'keyword',
-  ['elseif'] = 'keyword',
-  ['else'] = 'keyword',
-  ['return'] = 'keyword',
-  ['local'] = 'keyword',
-  ["getgenv"] = "builtin",
-  ["getrenv"] = "builtin",
-  ["getsenv"] = "builtin",
-  ["getreg"] = "builtin",
-  ["getgc"] = "builtin",
-  ["getinstances"] = "builtin",
-  ["getnilinstances"] = "builtin",
-  ["getscripts"] = "builtin",
-  ["getloadedmodules"] = "builtin",
-  ["getconnections"] = "builtin",
-  ["firesignal"] = "builtin",
-  ["fireclickdetector"] = "builtin",
-  ["firetouchinterest"] = "builtin",
-  ["getallthreads"] = "builtin",
-  ["gettenv"] = "builtin",
-  ["debug"] = "builtin",
-  ["repeat"] = "builtin",
-  ["getclipboard"] = "builtin"
+["and"] = "keyword";  ["break"] = "keyword";  ["do"] = "keyword";      ["else"] = "keyword";      ["elseif"] = "keyword";
+	["end"] = "keyword";  ["false"] = "keyword";  ["for"] = "keyword";     ["function"] = "keyword";  ["if"] = "keyword";
+	["in"] = "keyword";   ["local"] = "keyword";  ["nil"] = "keyword";     ["not"] = "keyword";       ["while"] = "keyword";
+	["or"] = "keyword";   ["repeat"] = "keyword"; ["return"] = "keyword";  ["then"] = "keyword";      ["true"] = "keyword";
+	["self"] = "keyword"; ["until"] = "keyword";
+["assert"] = "builtin";["collectgarbage"] = "builtin";["error"] = "builtin";["_G"] = "builtin";
+	["gcinfo"] = "builtin";["getfenv"] = "builtin";["getmetatable"] = "builtin";["ipairs"] = "builtin";
+	["loadstring"] = "builtin";["newproxy"] = "builtin";["next"] = "builtin";["pairs"] = "builtin";
+	["pcall"] = "builtin";["print"] = "builtin";["rawequal"] = "builtin";["rawget"] = "builtin";["rawset"] = "builtin";
+	["select"] = "builtin";["setfenv"] = "builtin";["setmetatable"] = "builtin";["tonumber"] = "builtin";
+	["tostring"] = "builtin";["type"] = "builtin";["unpack"] = "builtin";["_VERSION"] = "builtin";["xpcall"] = "builtin";
+	["delay"] = "builtin";["elapsedTime"] = "builtin";["require"] = "builtin";["spawn"] = "builtin";["tick"] = "builtin";
+	["time"] = "builtin";["typeof"] = "builtin";["UserSettings"] = "builtin";["wait"] = "builtin";["warn"] = "builtin";
+	["game"] = "builtin";["Enum"] = "builtin";["script"] = "builtin";["shared"] = "builtin";["workspace"] = "builtin";
+	["Axes"] = "builtin";["BrickColor"] = "builtin";["CFrame"] = "builtin";["Color3"] = "builtin";["ColorSequence"] = "builtin";
+	["ColorSequenceKeypoint"] = "builtin";["Faces"] = "builtin";["Instance"] = "builtin";["NumberRange"] = "builtin";
+	["NumberSequence"] = "builtin";["NumberSequenceKeypoint"] = "builtin";["PhysicalProperties"] = "builtin";
+	["Random"] = "builtin";["Ray"] = "builtin";["Rect"] = "builtin";["Region3"] = "builtin";["Region3int16"] = "builtin";
+	["TweenInfo"] = "builtin";["UDim"] = "builtin";["UDim2"] = "builtin";["Vector2"] = "builtin";["Vector3"] = "builtin";
+	["Vector3int16"] = "builtin";["next"] = "builtin";
+	["getgenv"] = "builtin";["getrenv"] = "builtin";["getsenv"] = "builtin";["getreg"] = "builtin";["getgc"] = "builtin";["getinstances"] = "builtin";["getnilinstances"] = "builtin";["getscripts"] = "builtin";["getloadedmodules"] = "builtin";["getconnections"] = "builtin";["firesignal"] = "builtin";["fireclickdetector"] = "builtin";["firetouchinterest"] = "builtin";["getallthreads"] = "builtin";["gettenv"] = "builtin";["getclipboard"] = "builtin";
+	["os"] = "builtin";
+	["debug"] = "builtin";
+	["math"] = "builtin";
+	["coroutine"] = "builtin";
+	["string"] = "builtin";
+	["table"] = "builtin";
+	["task"] = "builtin";
+
 }
 
 return words
