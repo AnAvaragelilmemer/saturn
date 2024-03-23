821d5e1ce1337cc36ccfa66ff0ca1b2b16b6de95
rtaxevasion <116945939+AnAvaragelilmemer@users.noreply.github.com>
2024-03-23 17:49:46 +0800

    Update Words.lua

--- assets/executorutils/Words.lua
+++ assets/executorutils/Words.lua
@@ -31,8 +31,24 @@ local words = {
   ['else'] = 'keyword',
   ['return'] = 'keyword',
   ['local'] = 'keyword',
-  ["getgenv()"] = "keyword",
-  ["getrenv()"] = "keyword"
+  ["getgenv"] = "builtin",
+  ["getrenv"] = "builtin",
+  ["getsenv"] = "builtin",
+  ["getreg"] = "builtin",
+  ["getgc"] = "builtin",
+  ["getinstances"] = "builtin",
+  ["getnilinstances"] = "builtin",
+  ["getscripts"] = "builtin",
+  ["getloadedmodules"] = "builtin",
+  ["getconnections"] = "builtin",
+  ["firesignal"] = "builtin",
+  ["fireclickdetector"] = "builtin",
+  ["firetouchinterest"] = "builtin",
+  ["getallthreads"] = "builtin",
+  ["gettenv"] = "builtin",
+  ["debug"] = "builtin",
+  ["repeat"] = "builtin",
+  ["getclipboard"] = "builtin"
 }
 
 return words
