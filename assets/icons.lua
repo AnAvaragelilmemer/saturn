4454e78dcbea25574b06948eb521158e4638da9f
rtaxevasion <116945939+AnAvaragelilmemer@users.noreply.github.com>
2024-04-01 18:34:03 +0800

    Update icons.lua

--- assets/icons.lua
+++ assets/icons.lua
@@ -1,10 +1,10 @@
 getgenv().iconlib = {}
 iconlib.icons = {
-	saturn = "rbxassetid://16967911244",--"rbxassetid://11419705585",
-	home = "rbxassetid://16967911244",--"rbxassetid://6026568198",
-	combat = "rbxassetid://16967911244",--"rbxassetid://9792632523",
-	visuals = "rbxassetid://16967911244",--"rbxassetid://9792631281",
-	movement = "rbxassetid://16967911244",--"rbxassetid://7743870731",
-	misc = "rbxassetid://16967911244",--"rbxassetid://9792634811"
+	saturn = "rbxassetid://11419705585",
+	home = "rbxassetid://6026568198",
+	combat = "rbxassetid://9792632523",
+	visuals = "rbxassetid://9792631281",
+	movement = "rbxassetid://7743870731",
+	misc = "rbxassetid://9792634811"
 }
 return iconlib
