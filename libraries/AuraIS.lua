d8f7c5e2f0cdfae080b4957a28f459ceb4bd13ca
rtaxevasion <116945939+AnAvaragelilmemer@users.noreply.github.com>
2025-02-22 20:19:45 +0800

    Update AuraIS.lua

--- libraries/AuraIS.lua
+++ libraries/AuraIS.lua
@@ -849,6 +849,11 @@ v4.CreateLibrary = function(v32, v33, v34)
 				v214.Visible = true;
 				v214.Parent = v102;
 				v105();
+				local paragraphchange = {}
+				function paragraphchange:Set(text)
+				v214.Content.Text = text
+				end 
+				return paragraphchange
 			end;
 			v101.CreateLabel = function(v222, v223)
 				local v224 = v39.Template.Label:Clone();
